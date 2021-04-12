735beb4771495a0aac0ed89b6764f5aaf8ac75fc
Saif Hossain <atonlymesaif@gmail.com>
2021-04-13 01:12:14 +0600

    add-task-screen.screen.dart added and ui completed.

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,10 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
         visualDensity: VisualDensity.adaptivePlatformDensity,
--- lib/screens/home-page.screens.dart
+++ lib/screens/home-page.screens.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'add-task-screen.screen.dart';
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Icon(
+            Icons.add,
+            color: Colors.white,
+            size: 40,
+          ),
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (_) => AddTaskScreen()));
+          },
+        ),
+      body: SafeArea(
+        child: Container(),
+      ),
+    );
   }
 }
